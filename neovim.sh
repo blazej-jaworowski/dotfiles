cb33b9df73ff2d3d72c185271b7d94f3ee7fc85a
Blazej Jaworowski <b.jaworowski@samsung.com>
2024-03-07 19:12:07 +0100

    Restructure

---
+++ neovim.sh
@@ -0,0 +1,33 @@
+#! /usr/bin/bash
+
+if [[ -z "$INSTALL_INSTRUCTION" ]]; then
+    echo "Please provide INSTALL_INSTRUCTION"
+    exit 1
+fi
+
+if [[ -z "$LOCAL_INSTALL_PATH" ]]; then
+    echo "Please provide LOCAL_INSTALL_PATH"
+    exit 1
+fi
+
+echo "Setting up neovim"
+
+# neovim build deps
+$INSTALL_INSTRUCTION build-essential cmake gettext unzip || exit 1
+
+# install neovim
+git clone -b "v0.9.5" --depth 1 https://github.com/neovim/neovim.git || exit 1
+cd neovim
+make CMAKE_BUILD_TYPE=Release CMAKE_INSTALL_PREFIX=$LOCAL_INSTALL_PATH -j || exit 1
+make install || exit 1
+
+# neovim plugin deps
+$INSTALL_INSTRUCTION ripgrep mercurial wget || exit 1
+
+# install neovim config
+mkdir -p "$HOME/.config"
+git clone https://github.com/blazej-jaworowski/nvim-distro.git $HOME/.config/nvim
+
+# "fix" nvim-treesitter issues
+rm $LOCAL_INSTALL_PATH/lib/parser/lua.so
+rm $LOCAL_INSTALL_PATH/lib/parser/vimdoc.so
